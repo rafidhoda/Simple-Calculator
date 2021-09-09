9c775f13f174d7f24a7134f7f73a824335883079
Rafid Hoda <rafidhoda@gmail.com>
2021-09-09 15:56:29 +0000

    Added third number

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ print("Simple Calculator")
 
 number1 = input("Enter first number: ")
 number2 = input("Enter second number: ")
+number3 = input("Enter second number: ")
 
 print("You entered " + number1)
 print("You entered " + number2)
+print("You entered " + number3)
 
-sum = int(number1) + int(number2)
+sum = int(number1) + int(number2) + int(number3)
 
 print("The sum is " + str(sum))
